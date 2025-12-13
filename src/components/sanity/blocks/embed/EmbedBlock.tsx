e62179733f6eb3dcaeadc99a448035fd545db478
James La Marre <jameslamarre@gmail.com>
2025-12-12 16:27:52 -0800

    fix: embed bg

--- src/components/sanity/blocks/embed/EmbedBlock.tsx
+++ src/components/sanity/blocks/embed/EmbedBlock.tsx
@@ -13,10 +13,10 @@ interface EmbedBlockProps
 export const EmbedBlock: FC<EmbedBlockProps> = ({ embed, className }) => {
   return (
     <Block className={classNames(className, 'w-full')}>
-      <div className="w-full max-w-textWrap min-h-[75svh] mt-y mx-auto">
+      <div className="w-full max-w-textWrap min-h-[50svh] mt-y mx-auto">
         {embed && (
           <div
-            className="h-full min-h-[75svh] bg-white"
+            className="h-full min-h-[50svh]"
             dangerouslySetInnerHTML={{ __html: embed }}
           />
         )}
