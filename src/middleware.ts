39f620a2aff7f3fdbb8df4a4110cf4be253213bf
James La Marre <jameslamarre@gmail.com>
2026-02-20 11:50:49 -0800

    test: robots

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,6 +36,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
+    '/robots.txt',
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
