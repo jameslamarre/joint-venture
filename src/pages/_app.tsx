01f45287528430c1f44024e323295c2d656bc35a
James La Marre <jameslamarre@gmail.com>
2026-02-27 07:36:49 -0800

    test: log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -41,6 +41,8 @@ function App({ Component, pageProps }: AppProps<{}>) {
   // eslint-disable-next-line no-console
   console.log('🔵 Site developed by https://masthead.dev 🟡')
 
+  console.log('Pageprops: ', pageProps)
+
   return (
     <ContextProvider>
       {type === 'psa' ? (
