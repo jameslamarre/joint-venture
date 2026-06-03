8f191581a277aa464aead34c301394d2552742f0
James La Marre <jameslamarre@gmail.com>
2026-06-03 13:33:57 -0700

    feat: new link type

--- studio/schemas/objects/link.ts
+++ studio/schemas/objects/link.ts
@@ -19,6 +19,7 @@ export default {
           to: [
             { type: 'page' },
             { type: 'project' },
+            { type: 'job' },
             { type: 'micrositePage' },
           ],
           weak: true,
